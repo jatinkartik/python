6a899bf75ab025a4b9965e06f56b865670f62168
jatinkartik <jatintyagi43625@gmail.com>
2021-03-24 23:18:12 +0530

    modified

--- 08_py_python2automationmacchanger.py
+++ 08_py_python2automationmacchanger.py
@@ -1,8 +1,10 @@
+ # #!/usr/bin/env python2
+
 import subprocess
 # x = input("enter the vlaue of mac")
-interface = input("Interface >")
-vl = input("New MAC >")
-print("[+] changing the mac for "+interface+" to "+vl)
+interface = "eth0"
+vl = raw_input("enter the value of mac address")
+# print("[+] changing the mac for "+interface+" to "+vl)
 subprocess.call("ifconfig "+interface+" down",shell=True)
 subprocess.call("sudo ifconfig "+interface+" hw ether "+vl+" ",shell=True)
 subprocess.call("ifconfig "+interface+" up",shell=True)
