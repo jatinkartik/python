f6b4ff8fcf92fd40cdbf3743f5be11cb2cba294e
jatinkartik <jatintyagi43625@gmail.com>
2021-06-06 20:31:55 +0530

    new change

--- 02_pyhton.py
+++ 02_pyhton.py
@@ -1,6 +1,21 @@
-#!/usr/bin/env python2
-from playsound import playsound
-# /home/jatintyagi/Downloads/faded.mp3 ---> path to file
-print("songs start")
-playsound('/home/jatintyagi/Desktop/python/faded.mp3')
-print("songs ends")
+# #!/usr/bin/env python2
+# from playsound import playsound
+# # /home/jatintyagi/Downloads/faded.mp3 ---> path to file
+# print("songs start")
+# playsound('/home/jatintyagi/Desktop/python/faded.mp3')
+# print("songs ends")
+
+
+
+import optparse
+import subprocess
+
+parser = optparse.OptionParser()
+parser.add_option("-i","--interface",dest="interface",help="interface of the network")
+parser.add_option("-m","--new_mac"dest="new_mac",help="new mac address")
+(options,interface) = parser.parse_args()
+
+subprocess.call("sudo ifconfig", "eth0", "down",shell=True)
+subprocess.call("sudo ifconfig", "eth0", "hw", "ether","00:11:33:44:55:55",shell=True)
+subprocess.call("sudo ifconfig", "eth0", "up",shell=True)
+
---
+++ 10_pyhton_if_else.py
@@ -0,0 +1,8 @@
+print("hello jatin kartik tyagi enter the no")
+int no = input("enter the no yaar")
+if no < 5:
+    print("teek hoo bhai")
+elif no==10:
+    print("boht badia")
+else:
+    print("heavy driver")
